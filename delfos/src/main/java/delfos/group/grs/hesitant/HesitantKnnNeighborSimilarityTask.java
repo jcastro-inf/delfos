1410207ebba3e1cedb17f1f19c6eba93a76330da
Jorge Castro Gallardo <jcastro@ujaen.es>
2015-08-06 13:44:04 +0200

    Debug multithread: executes everything in the run method, single thread

---
+++ delfos/src/main/java/delfos/group/grs/hesitant/HesitantKnnNeighborSimilarityTask.java
@@ -0,0 +1,53 @@
+package delfos.rs.collaborativefiltering.knn.memorybased;
+
+import delfos.common.exceptions.dataset.users.UserNotFound;
+import delfos.common.parallelwork.Task;
+import delfos.dataset.basic.rating.Rating;
+import delfos.dataset.basic.rating.RatingsDataset;
+import delfos.rs.collaborativefiltering.profile.Neighbor;
+
+/**
+ * Clase que almacena los datos necesarios para ejecutar paralelamente el
+ * cálculo de la similitud con un vecino.
+ *
+ * @author Jorge Castro Gallardo
+ *
+ * @version 14-Noviembre-2013
+ */
+public class KnnMemoryTask extends Task {
+
+    public final int idUser;
+    public final int idNeighbor;
+    public KnnMemoryBasedCFRS rs;
+    public RatingsDataset<? extends Rating> ratingsDataset;
+    public Neighbor neighbor = null;
+
+    public KnnMemoryTask(RatingsDataset<? extends Rating> ratingsDataset, int idUser, int idNeighbor, KnnMemoryBasedCFRS rs) throws UserNotFound {
+        this.ratingsDataset = ratingsDataset;
+        this.idUser = idUser;
+        this.idNeighbor = idNeighbor;
+        this.rs = rs;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+
+        str.append("idUser ---------> ").append(idUser).append("\n");
+        str.append("idNeighbor -----> ").append(idNeighbor).append("\n");
+        str.append("rs -------------> ").append(rs.getAlias()).append("\n");
+        str.append("\n").append(rs.getNameWithParameters());
+
+        return str.toString();
+    }
+
+    public void setNeighbor(Neighbor neighbor) {
+        this.neighbor = neighbor;
+        rs = null;
+        ratingsDataset = null;
+    }
+
+    public Neighbor getNeighbor() {
+        return neighbor;
+    }
+}
