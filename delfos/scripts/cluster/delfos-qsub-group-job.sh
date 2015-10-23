a386eb2ba39f367d71ce7e29d5f1663582dcc818
Jorge Castro Gallardo <jcastro@ujaen.es>
2015-10-23 10:57:58 +0200

    group-job always executed, even if it was already done

--- delfos/scripts/cluster/delfos-qsub-group-job.sh
+++ delfos/scripts/cluster/delfos-qsub-group-job.sh
@@ -13,11 +13,11 @@ idJob=${PBS_JOBID%%.*}
 experimentFolderNoBars=${experimentFolder//"/"/.}
 resultsFile=${experimentFolder}results/aggregateResults.xls
 
-if [ -a $resultsFile ] 
-then
-   echo "Experimento $experimentFolder ejecutado aqui" >  $PBS_JOBNAME.o$idJob.$HOSTNAME.${experimentFolderNoBars}.yaHabiaSidoEjecutado   
-else
+#if [ -a $resultsFile ]
+#then
+#   echo "Experimento $experimentFolder ejecutado aqui" >  $PBS_JOBNAME.o$idJob.$HOSTNAME.${experimentFolderNoBars}.yaHabiaSidoEjecutado
+#else
    echo "Experimento $experimentFolder ejecutado aqui" >  $PBS_JOBNAME.o$idJob.$HOSTNAME.${experimentFolderNoBars}ejecutando
    ~/java-8-oracle/bin/java -Xms8g -jar delfos.jar --execute-group-xml -seed 123456 -directory ${experimentFolder} -num-exec $numExec
    echo "Experimento $experimentFolder ejecutado aqui" >  $PBS_JOBNAME.o$idJob.$HOSTNAME.${experimentFolderNoBars}finalizado
-fi
+#fi
