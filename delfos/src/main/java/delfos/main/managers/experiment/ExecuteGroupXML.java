4a977235c9f86c62ef3b336d420616c588459397
Jorge Castro Gallardo <jcastro@ujaen.es>
2015-10-27 11:04:15 +0100

    Added feature to execute only if it was not finished or if the number of executions requested is greater than the already executed. It is also possible to force execution using the command line parameter --force-execution

--- delfos/src/main/java/delfos/main/managers/experiment/ExecuteGroupXML.java
+++ delfos/src/main/java/delfos/main/managers/experiment/ExecuteGroupXML.java
@@ -3,11 +3,15 @@ package delfos.main.managers.experiment;
 import delfos.ConsoleParameters;
 import delfos.ERROR_CODES;
 import delfos.UndefinedParameterException;
+import delfos.common.Global;
 import delfos.common.exceptions.dataset.CannotLoadContentDataset;
 import delfos.common.exceptions.dataset.CannotLoadRatingsDataset;
 import delfos.group.casestudy.fromxmlfiles.GroupXMLexperimentsExecution;
+import delfos.group.io.xml.casestudy.GroupCaseStudyXML;
 import delfos.main.managers.CaseUseMode;
 import java.io.File;
+import java.io.IOException;
+import org.jdom2.JDOMException;
 
 /**
  *
@@ -24,10 +28,37 @@ public class ExecuteGroupXML extends CaseUseMode {
     public static final String XML_DIRECTORY = ExecuteXML.XML_DIRECTORY;
     public static final String SEED_PARAMETER = ExecuteXML.SEED_PARAMETER;
     public static final String NUM_EXEC_PARAMETER = ExecuteXML.NUM_EXEC_PARAMETER;
+    public static final String FORCE_EXECUTION = ExecuteXML.FORCE_EXECUTION;
 
     @Override
     public String getModeParameter() {
         return MODE_PARAMETER;
+    }
+
+    private boolean isForceExecution(ConsoleParameters consoleParameters) {
+        return consoleParameters.isFlagDefined(FORCE_EXECUTION);
+    }
+
+    private boolean isAlreadyExecuted(File xmlExperimentsDirectory) {
+        return isResultAggregatedXMLPresent(xmlExperimentsDirectory);
+    }
+
+    private boolean isResultAggregatedXMLPresent(File xmlExperimentsDirectory) {
+        File resultAggregatedXML = new File(xmlExperimentsDirectory.getPath() + File.separator + "results" + File.separator + xmlExperimentsDirectory.getName() + "_AGGR.xml");
+
+        return resultAggregatedXML.exists();
+    }
+
+    private boolean isNumExecGreaterThanTheExisting(File xmlExperimentsDirectory, int NUM_EJECUCIONES) {
+
+        File resultAggregatedXML = new File(xmlExperimentsDirectory.getPath() + File.separator + "results" + File.separator + xmlExperimentsDirectory.getName() + "_AGGR.xml");
+        try {
+            int extractResultNumExec = GroupCaseStudyXML.extractResultNumExec(resultAggregatedXML);
+            return NUM_EJECUCIONES > extractResultNumExec;
+        } catch (JDOMException | IOException ex) {
+            ERROR_CODES.CANNOT_READ_CASE_STUDY_XML.exit(ex);
+        }
+        return false;
     }
 
     private static class Holder {
@@ -45,12 +76,18 @@ public class ExecuteGroupXML extends CaseUseMode {
     @Override
     public void manageCaseUse(ConsoleParameters consoleParameters) {
         try {
-            String xmlExperimentsDirectory = consoleParameters.getValue(ExecuteGroupXML.XML_DIRECTORY);
+            File xmlExperimentsDirectory = new File(consoleParameters.getValue(ExecuteGroupXML.XML_DIRECTORY));
             int NUM_EJECUCIONES = getNumExecutions(consoleParameters);
             long SEED = getSeed(consoleParameters);
+            boolean forceReExecution = isForceExecution(consoleParameters);
 
             consoleParameters.printUnusedParameters(System.err);
-            manageCaseUse(xmlExperimentsDirectory, xmlExperimentsDirectory + File.separator + "dataset" + File.separator, NUM_EJECUCIONES, SEED);
+
+            if (shouldExecuteTheExperiment(xmlExperimentsDirectory, NUM_EJECUCIONES, forceReExecution)) {
+                manageCaseUse(xmlExperimentsDirectory, xmlExperimentsDirectory + File.separator + "dataset" + File.separator, NUM_EJECUCIONES, SEED);
+            } else {
+                Global.showMessageTimestamped("The experiment was already executed. (" + xmlExperimentsDirectory.getPath() + ")");
+            }
         } catch (UndefinedParameterException ex) {
             consoleParameters.printUnusedParameters(System.err);
         }
@@ -84,10 +121,10 @@ public class ExecuteGroupXML extends CaseUseMode {
         return NUM_EJECUCIONES;
     }
 
-    public static void manageCaseUse(String experimentsDirectory, String datasetDirectory, int numExecutions, long seed) {
+    public static void manageCaseUse(File experimentsDirectory, String datasetDirectory, int numExecutions, long seed) {
         try {
             GroupXMLexperimentsExecution execution = new GroupXMLexperimentsExecution(
-                    experimentsDirectory,
+                    experimentsDirectory.getPath(),
                     datasetDirectory,
                     numExecutions,
                     seed);
@@ -96,6 +133,18 @@ public class ExecuteGroupXML extends CaseUseMode {
             ERROR_CODES.CANNOT_LOAD_CONTENT_DATASET.exit(ex);
         } catch (CannotLoadRatingsDataset ex) {
             ERROR_CODES.CANNOT_LOAD_RATINGS_DATASET.exit(ex);
+        }
+    }
+
+    public boolean shouldExecuteTheExperiment(File xmlExperimentsDirectory, int NUM_EJECUCIONES, boolean forceReExecution) {
+        if (forceReExecution) {
+            return true;
+        } else if (!isAlreadyExecuted(xmlExperimentsDirectory)) {
+            return true;
+        } else if (isNumExecGreaterThanTheExisting(xmlExperimentsDirectory, NUM_EJECUCIONES)) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
