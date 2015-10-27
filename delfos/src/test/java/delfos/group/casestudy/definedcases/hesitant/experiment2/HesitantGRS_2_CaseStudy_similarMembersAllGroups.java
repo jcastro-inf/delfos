b7bd687fa9048a915e94c2819674c8b389045cb4
Jorge Castro Gallardo <jcastro@ujaen.es>
2015-10-27 19:40:31 +0100

    HesitantGRS.Experiment2 updated to test similar and dissimilar group members

---
+++ delfos/src/test/java/delfos/group/casestudy/definedcases/hesitant/experiment2/HesitantGRS_2_CaseStudy_similarMembersAllGroups.java
@@ -0,0 +1,134 @@
+package delfos.group.casestudy.definedcases.hesitant.experiment2;
+
+import delfos.Constants;
+import delfos.common.FileUtilities;
+import delfos.common.Global;
+import delfos.configureddatasets.ConfiguredDatasetLoader;
+import delfos.constants.DelfosTest;
+import delfos.dataset.basic.loader.types.DatasetLoader;
+import delfos.dataset.basic.rating.RelevanceCriteria;
+import delfos.experiment.casestudy.cluster.TuringPreparator;
+import delfos.group.casestudy.GroupCaseStudy;
+import delfos.group.casestudy.defaultcase.DefaultGroupCaseStudy;
+import delfos.group.experiment.validation.groupformation.GroupFormationTechnique;
+import delfos.group.experiment.validation.groupformation.SimilarMembers;
+import delfos.group.experiment.validation.predictionvalidation.NoPredictionProtocol;
+import delfos.group.experiment.validation.validationtechniques.HoldOutGroupRatedItems;
+import delfos.group.factories.GroupEvaluationMeasuresFactory;
+import delfos.group.grs.GroupRecommenderSystem;
+import delfos.group.grs.hesitant.HesitantKnnGroupUser;
+import es.jcastro.hesitant.similarity.HesitantPearson;
+import es.jcastro.hesitant.similarity.factory.HesitantSimilarityFactory;
+import java.io.File;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.Test;
+
+public class HesitantGRS_2_CaseStudy_similarMembersAllGroups extends DelfosTest {
+
+    public HesitantGRS_2_CaseStudy_similarMembersAllGroups() {
+    }
+
+    public static final long SEED_VALUE = 123456L;
+
+    File experimentDirectory = new File(Constants.getTempDirectory().getAbsolutePath() + File.separator
+            + "HesitantGRS.experiment2" + File.separator
+            + HesitantGRS_2_CaseStudy_similarMembersAllGroups.class.getSimpleName() + File.separator);
+
+    private Collection<GroupFormationTechnique> getGroupFormationTechnique() {
+        return Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 50, 100, 200, 500).stream()
+                .map((groupSize) -> {
+                    GroupFormationTechnique gft = new SimilarMembers(groupSize, 1);
+                    return gft;
+                }).collect(Collectors.toList());
+
+    }
+
+    private Collection<ConfiguredDatasetLoader> getDatasetLoader() {
+        return Arrays.asList(new ConfiguredDatasetLoader("ml-100k"));
+    }
+
+    private List<GroupRecommenderSystem> getGRSs() {
+        int neighborhoodSize = 100;
+
+        List<GroupRecommenderSystem> ret = new ArrayList<>();
+
+        ret.addAll(HesitantSimilarityFactory.getAll()
+                .stream()
+                .map((hesitantSimilarity) -> {
+
+                    DecimalFormat format = new DecimalFormat("000");
+
+                    HesitantKnnGroupUser grs = new HesitantKnnGroupUser();
+                    grs.setAlias(hesitantSimilarity.getName() + "_neighborhoodSize=" + format.format(neighborhoodSize));
+                    grs.setParameterValue(HesitantKnnGroupUser.NEIGHBORHOOD_SIZE, neighborhoodSize);
+                    grs.setParameterValue(HesitantKnnGroupUser.HESITANT_SIMILARITY_MEASURE, hesitantSimilarity);
+                    return grs;
+
+                }).collect(Collectors.toList()));
+
+        {
+
+            HesitantPearson hesitantSimilarity = new HesitantPearson();
+            NumberFormat format = new DecimalFormat("000");
+            HesitantKnnGroupUser hesitantGRS = new HesitantKnnGroupUser();
+
+            hesitantGRS.setAlias(hesitantSimilarity.getName() + "_deleteRepeated" + "_neighborhoodSize=" + format.format(neighborhoodSize));
+            hesitantGRS.setParameterValue(HesitantKnnGroupUser.NEIGHBORHOOD_SIZE, neighborhoodSize);
+            hesitantGRS.setParameterValue(HesitantKnnGroupUser.HESITANT_SIMILARITY_MEASURE, hesitantSimilarity);
+            hesitantGRS.setParameterValue(HesitantKnnGroupUser.DELETE_REPEATED, true);
+
+            ret.add(hesitantGRS);
+        }
+        return ret;
+    }
+
+    public void createCaseStudyXML() {
+
+        TuringPreparator turingPreparator = new TuringPreparator();
+
+        List<GroupCaseStudy> groupCaseStudys = new ArrayList<>();
+
+        for (GroupFormationTechnique groupFormationTechnique : getGroupFormationTechnique()) {
+            for (GroupRecommenderSystem groupRecommenderSystem : getGRSs()) {
+                DefaultGroupCaseStudy groupCaseStudy = new DefaultGroupCaseStudy(
+                        null,
+                        groupRecommenderSystem,
+                        groupFormationTechnique,
+                        new HoldOutGroupRatedItems(),
+                        new NoPredictionProtocol(),
+                        GroupEvaluationMeasuresFactory.getInstance().getAllClasses(),
+                        new RelevanceCriteria(4),
+                        1,
+                        SEED_VALUE
+                );
+
+                groupCaseStudy.setAlias(
+                        "_methodHash=" + groupCaseStudy.hashCodeWithoutGroupRecommenderSystem()
+                        + "_" + groupRecommenderSystem.getAlias()
+                        + "_allHash=" + groupCaseStudy.hashCode()
+                );
+                groupCaseStudys.add(groupCaseStudy);
+            }
+        }
+
+        turingPreparator.prepareGroupExperiment(
+                experimentDirectory,
+                groupCaseStudys,
+                getDatasetLoader().toArray(new DatasetLoader[0]));
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+        FileUtilities.deleteDirectoryRecursive(experimentDirectory);
+        createCaseStudyXML();
+        Global.show("This case study has " + new TuringPreparator()
+                .sizeOfAllExperimentsInDirectory(experimentDirectory)
+                + " experiments");
+    }
+}
