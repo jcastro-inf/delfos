e2c5001ad462a7b717c5ce83f45934dcb96bb4dd
Jorge Castro Gallardo <jcastro@ujaen.es>
2015-10-28 10:47:25 +0100

    Store the log in the experiment folder, to easy experiments failures

--- delfos/scripts/cluster/delfos-qsub-group-job.sh
+++ delfos/scripts/cluster/delfos-qsub-group-job.sh
@@ -12,7 +12,13 @@ idJob=${PBS_JOBID%%.*}
 
 experimentFolderNoBars=${experimentFolder//"/"/.}
 
-echo "Experimento $experimentFolder ejecutado aqui" >  $PBS_JOBNAME.o$idJob.$HOSTNAME.${experimentFolderNoBars}ejecutando
-~/java-8-oracle/bin/java -Xms16g -Xmx32g -jar delfos.jar --execute-group-xml -seed 123456 -directory ${experimentFolder} -num-exec $numExec
-echo "Experimento $experimentFolder ejecutado aqui" >  $PBS_JOBNAME.o$idJob.$HOSTNAME.${experimentFolderNoBars}finalizado
+logDirectory=$experimentFolder"/log/"
+if [ ! -d $logDirectory ]; then
+   mkdir $logDirectory
+fi
 
+DATE=`date +%Y-%m-%d_%H.%M.%S`
+logFileStd=$logDirectory$DATE"-log-std.txt"
+logFileErr=$logDirectory$DATE"-log-err.txt"
+
+~/java-8-oracle/bin/java -Xms16g -Xmx32g -jar delfos.jar --execute-group-xml -seed 123456 -directory ${experimentFolder} -num-exec $numExec > $logFileStd 2> $logFileErr
