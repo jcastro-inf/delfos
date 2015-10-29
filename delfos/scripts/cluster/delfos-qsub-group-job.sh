35bc15f6bf6bf46dedfea53412abee10610374a4
Jorge Castro Gallardo <jcastro@ujaen.es>
2015-10-29 10:59:52 +0100

    added logging information such as date start, date finished

--- delfos/scripts/cluster/delfos-qsub-group-job.sh
+++ delfos/scripts/cluster/delfos-qsub-group-job.sh
@@ -17,19 +17,25 @@ if [ ! -d $logDirectory ]; then
    mkdir $logDirectory
 fi
 
-DATE=`date +%Y-%m-%d_%H.%M.%S`
-logFileStd=$logDirectory$DATE"-log-std.txt"
-logFileErr=$logDirectory$DATE"-log-err.txt"
+DATE_START=`date +%Y-%m-%d_%H.%M.%S`
+logFileStd=$logDirectory$DATE_START"-log-std.txt"
+logFileErr=$logDirectory$DATE_START"-log-err.txt"
 
-echo "$DATE executing" $experimentFolder
-echo "$DATE executing" $experimentFolder >> $logFileStd 2>> $logFileErr
+echo "$DATE_START executing" $experimentFolder
+echo "$DATE_START executing" $experimentFolder >> $logFileStd 2>> $logFileErr
 
-echo "$DATE Executed in node `(hostname)`"
-echo "$DATE Executed in node `(hostname)`" >> $logFileStd 2>> $logFileErr
+echo "$DATE_START Executed in node `(hostname)`"
+echo "$DATE_START Executed in node `(hostname)`" >> $logFileStd 2>> $logFileErr
 
 
 ~/java-8-oracle/bin/java -Xms16g -Xmx32g -jar delfos.jar --execute-group-xml -seed 123456 -directory ${experimentFolder} -num-exec $numExec >> $logFileStd 2>> $logFileErr
 
+cat $logFileStd >&1
+cat $logFileErr >&2
 
-echo "$DATE Finished" $experimentFolder
-echo "$DATE Finished" $experimentFolder >> $logFileStd 2>> $logFileErr
+
+echo "$DATE_START executing" $experimentFolder
+echo "$DATE_START executing" $experimentFolder >> $logFileStd 2>> $logFileErr
+DATE_FINISH=`date +%Y-%m-%d_%H.%M.%S`
+echo "$DATE_FINISH Finished" $experimentFolder
+echo "$DATE_FINISH Finished" $experimentFolder >> $logFileStd 2>> $logFileErr
