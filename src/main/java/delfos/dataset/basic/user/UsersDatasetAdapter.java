3a56bd5e3921ad891d58eb1463d60dcd585d7816
Jorge Castro Gallardo <jcastro@ujaen.es>
2016-02-17 15:07:11 +1100

    New hashCode methods for contentDataset and usersDataset

--- src/main/java/delfos/dataset/basic/user/UsersDatasetAdapter.java
+++ src/main/java/delfos/dataset/basic/user/UsersDatasetAdapter.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2016 jcastro
  *
  * This program is free software: you can redistribute it and/or modify
@@ -20,9 +20,13 @@ import delfos.common.exceptions.dataset.entity.EntityNotFound;
 import delfos.common.exceptions.dataset.users.UserNotFound;
 import delfos.dataset.basic.features.CollectionOfEntitiesWithFeaturesDefault;
 import delfos.dataset.basic.features.Feature;
+import delfos.dataset.basic.rating.Rating;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 /**
  *
@@ -75,4 +79,41 @@ public class UsersDatasetAdapter extends CollectionOfEntitiesWithFeaturesDefault
         }
         return _entitiesById.toString();
     }
+
+    private Integer hashCodeBuffer = null;
+
+    @Override
+    public int hashCode() {
+        if (hashCodeBuffer == null) {
+            hashCodeBuffer = hashCode(this);
+        }
+        return hashCodeBuffer;
+    }
+
+    public static <RatingType extends Rating> int hashCode(UsersDataset usersDataset) {
+        HashCodeBuilder hashCodeBuilder = new HashCodeBuilder(37, 11);
+
+        List<Integer> usersSorted = usersDataset.allIDs().stream().collect(Collectors.toList());
+        usersSorted.sort((i1, i2) -> Integer.compare(i1, i2));
+
+        Feature[] features = usersDataset.getFeatures();
+
+        for (int idUser : usersSorted) {
+            User user = usersDataset.get(idUser);
+
+            hashCodeBuilder.append(idUser);
+            hashCodeBuilder.append(user.getName());
+            for (Feature feature : features) {
+                if (user.getFeatures().contains(feature)) {
+                    Object featureValue = user.getFeatureValue(feature);
+                    hashCodeBuilder.append(feature.getName());
+                    hashCodeBuilder.append(feature.getType());
+                    hashCodeBuilder.append(featureValue);
+                }
+            }
+        }
+
+        return hashCodeBuilder.hashCode();
+    }
+
 }
