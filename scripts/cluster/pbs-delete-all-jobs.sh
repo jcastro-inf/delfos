816138d7aa8208a0700b3f890d9f66312c7c7313
Jorge Castro Gallardo <jcastro@ujaen.es>
2016-03-01 13:59:29 +1100

    The delete all jobs script deletes queued first and then running jobs. This avoids task entering the running mode and being killed immediately

--- scripts/cluster/pbs-delete-all-jobs.sh
+++ scripts/cluster/pbs-delete-all-jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-qstat | grep sinbad2 | egrep -o '[0-9][0-9][0-9][0-9][0-9][0-9]'| while read -r jobId ; do
-   qdel $jobId
-   echo "deleted job $jobId"
-   sleep 0.2
-done
+echo "Deleting unfinished jobs"
+./pbs-delete-unfinished-jobs.sh
+echo "Deleting unfinished done. Now delete running jobs"
+./pbs-delete-running-jobs.sh
+
