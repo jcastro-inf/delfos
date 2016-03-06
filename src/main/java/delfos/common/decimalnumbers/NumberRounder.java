a2c3b872b23b70f46bec2561ad62db52bc2ef7fe
Jorge Castro Gallardo <jcastro@ujaen.es>
2016-03-06 22:23:48 +1100

    Fix bug in numberRounder

--- src/main/java/delfos/common/decimalnumbers/NumberRounder.java
+++ src/main/java/delfos/common/decimalnumbers/NumberRounder.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2016 jcastro
  *
  * This program is free software: you can redistribute it and/or modify
@@ -30,7 +30,7 @@ public class NumberRounder {
      * Redondea el valor indicado al número de decimales indicado.
      *
      * @param value Valor que se quiere redondear.
-     * @param numDecimals Número de decimales que se consideran.
+     * @param numDecimals Número de decimales a los que se redondea.
      * @return Valor redondeado
      */
     public static double round(Number value, int numDecimals) {
@@ -45,13 +45,17 @@ public class NumberRounder {
 
         double pow = Math.pow(10, numDecimals);
 
-        int intValue = (int) (value.doubleValue() * pow);
+        long intValue = Math.round(value.doubleValue() * pow);
 
         double ret = intValue / pow;
+
+        if (Math.abs(ret - value.doubleValue()) > 0.001) {
+            throw new IllegalStateException("The value rounded is very different from the original");
+        }
         return ret;
     }
 
-    public static String round_str(Number value) {
+    public static String round_str(Number value, int numDigits) {
 
         double doubleValue = value.doubleValue();
         if (Double.isInfinite(doubleValue)) {
@@ -61,13 +65,30 @@ public class NumberRounder {
         if (Double.isNaN(doubleValue)) {
             return "NaN";
         }
+        if (numDigits < 0) {
+            throw new IllegalArgumentException("Number of decimals can't be negative.");
+        }
 
         DecimalFormat df = new DecimalFormat();
 
-        df.setMinimumFractionDigits(8);
-        df.setMaximumFractionDigits(8);
+        df.setMinimumFractionDigits(numDigits);
+        df.setMaximumFractionDigits(numDigits);
 
         String format = df.format(value);
         return format;
+    }
+
+    public static String round_str(Number value) {
+        return round_str(value, 8);
+    }
+
+    /**
+     * Redondea el valor indicado al número de decimales indicado.
+     *
+     * @param value Valor que se quiere redondear.
+     * @return Valor redondeado
+     */
+    public static double round(Number value) {
+        return round(value, 8);
     }
 }
