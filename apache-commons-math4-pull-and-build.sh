8796105ac96e290e4e86e098443d7a038629c722
Jorge Castro Gallardo <jcastro@ujaen.es>
2017-01-05 11:48:22 +0100

    Fix apache commons math4 compilation script to go back to master branch before pulling

--- apache-commons-math4-pull-and-build.sh
+++ apache-commons-math4-pull-and-build.sh
@@ -15,11 +15,12 @@ echo $commonsMath4Directory "The directory"
 
 if [ ! -d "$commonsMath4Directory" ]; then
 	echo "Does not exists"
-        git clone https://github.com/apache/commons-math.git
+    git clone https://github.com/apache/commons-math.git
 else
-        echo "It exists, pull only"
+    echo "It exists, pull only"
 	cd $commonsMath4Directory
-        git pull origin master
+    git checkout master
+    git pull origin master
 fi
 
 echo "Now i should compile"
